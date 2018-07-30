c06a9d0ad6f8d0a047f3559ada778e024951374f
Shannon Lal <shannonlal@hotmail.com>
2018-07-29 20:53:01 -0400

    Refactor Table into stateful component

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -12,30 +12,39 @@ import {  Post } from 'react-axios';
  * @author [Shannon Lal](https://github.com/shannonlal)
  */
 
+class Table extends React.Component{
 
-function Table({width, height, columnNames, rows}){
+    constructor(props){
+        super(props);
+        
+        this.setState(
+            {width:props.width, height:props.height, 
+                columnNames:[], rows:[]});
+    }
 
-    //https://css-tricks.com/fitting-text-to-a-container/
-    const tableHeight = {
-        height:(height - 50)
-    };
+    render(){
+        //https://css-tricks.com/fitting-text-to-a-container/
+        const tableHeight = {
+            height:(this.state.height - 50)
+        };
 
-    console.log( `Table Container Height ${height}`);
-            /*<div className="headercontainer" > **/
-    return ( 
+        console.log( `Table Container Height ${height}`);
+                /*<div className="headercontainer" > **/
+        return ( 
 
-            <div className="tablecontainer" style={tableHeight}>
-                <table>
-                    <Header columnNames={columnNames} width={width} height={height} />
-                    {   
-                        rows.map( (row,index) =>{
-                            return(<Row key={index} columnNames={columnNames} row={row} width={width}/>)
-                        })
-                    }
-                </table>
+                <div className="tablecontainer" style={tableHeight}>
+                    <table>
+                        <Header columnNames={this.state.columnNames} width={this.state.width} height={this.state.height} />
+                        {   
+                            this.state.rows.map( (row,index) =>{
+                                return(<Row key={index} columnNames={this.state.columnNames} row={row} width={this.state.width}/>)
+                            })
+                        }
+                    </table>
 
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 Table.propTypes = {
